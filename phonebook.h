7032114f1077675a56a777fe7242e9352b58b43d
JacobLong2005 <jlong94@ut.utm.edu>
2025-03-08 18:01:00 -0600

    Create phonebook.h

---
+++ phonebook.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+class phonebook(){
+  public:
+    ReadPhoneFile();
+    WriteFile();
+    PrintPhone();
+    SearchName();
+    SearchNumber();
+    AddUser();
+    DeleteUser();
+    Exit();
+  private();
+    string* FirstN[];
+    string* LastN[];
+    int* number[];
+}
